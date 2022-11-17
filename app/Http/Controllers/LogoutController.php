966f66c4a7abf7e786bdbde1467ac54ea4c7b54f
Ivan <ivanbisc12@gmail.com>
2022-11-17 20:00:39 -0300

    logout

--- app/Http/Controllers/LogoutController.php
+++ app/Http/Controllers/LogoutController.php
@@ -3,13 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Auth;
 
 class LogoutController extends Controller
 {
-    public function cerrar(Request $request){
-        if(session('user')){
-            $request->session->forget('user');
+    public function logout(Request $request){
+
+        if(Auth::check()) {
+            Session::flush();
+            Auth::logout();
         }
-        return redirect()->route('/');
+        return redirect()->route('principal');
     }
 }
