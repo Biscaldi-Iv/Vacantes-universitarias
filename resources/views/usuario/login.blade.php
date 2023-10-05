b9b82fd48e3f460559c09161d6143d02799772be
AngelSony <angelfol@outlook.com>
2023-10-05 17:15:25 -0300

    Agregado recaptcha Login

--- resources/views/usuario/login.blade.php
+++ resources/views/usuario/login.blade.php
@@ -2,19 +2,20 @@
 
 @section('titulo')
     <title>Inicio de sesion</title>
+    <script src="https://www.google.com/recaptcha/api.js"></script>
 @endsection
 
 @section('breadcrumb')
     <p>
-        /<a class="breadcrumb"href="/">Página principal</a>
-        /<a class="breadcrumb"href="/login">Inicio de sesion</a>
+        /<a class="breadcrumb" href="/">Página principal</a>
+        /<a class="breadcrumb" href="/login">Inicio de sesion</a>
     </p>
 @endsection
 
 @section('contenido')
     <div class="container mt-4 shadow"
         style="width:30%; padding:40px; margin-top: 30%; margin-bottom: 10%; border-radius:10px;">
-        <form action="/login" method="POST" style="margin-top:20px; margin-bottom:20px;">
+        <form action="/login" method="POST" style="margin-top:20px; margin-bottom:20px;" id="form-login">
             @csrf
             <div class="mb-3">
                 <label for="email" class="form-label">Correo electronico</label>
@@ -25,12 +26,22 @@
                 <label for="password" class="form-label">Contraseña</label>
                 <input type="password" class="form-control" name="password" id="password" placeholder="Contraseña">
             </div>
+            <div class="mb-3">
+                <div class="g-recaptcha" data-sitekey={{ env('CAPTCHA_SITE_KEY') }}></div>
+            </div>
             <button type="submit" class="btn btn-primary w-100">Iniciar sesion</button>
-            <button type="button" class="btn btn-light w-100" name="registrarse" onclick="location.href='/registrarse'">Registrarse</button>
-            <button type="button" class="btn btn-light w-100" name="reestablecer" onclick="location.href='/contraseña/olvidada'">Has olvidado tu contraseña?</a>
+            <button type="button" class="btn btn-light w-100" name="registrarse"
+                onclick="location.href='/registrarse'">Registrarse</button>
+            <button type="button" class="btn btn-light w-100" name="reestablecer"
+                onclick="location.href='/contraseña/olvidada'">Has olvidado tu contraseña?</a>
         </form>
     </div>
 @endsection
 
 @section('scripts')
+    <script>
+        function onSubmit(token) {
+            document.getElementById("form-login").submit();
+        }
+    </script>
 @endsection
