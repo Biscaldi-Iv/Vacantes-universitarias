7a3074e596be098ef2d8b96133a3b7699c27de7a
AngelSony <angelfol@outlook.com>
2023-10-05 17:24:50 -0300

    Agregado captcha Registro

--- app/Http/Controllers/RegistroController.php
+++ app/Http/Controllers/RegistroController.php
@@ -11,36 +11,44 @@ use App\Models\PersonalUniversidad;
 use App\Models\Universidad;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\DB;
 
 class RegistroController extends Controller
 {
     //
-    public function show(){
-        if(Auth::check()) {
-            return redirect()->route('principal')->with('success','Usted ya ha iniciado sesion!');
+    public function show()
+    {
+        if (Auth::check()) {
+            return redirect()->route('principal')->with('success', 'Usted ya ha iniciado sesion!');
         }
         return view('usuario.registrarse');
     }
 
-    public function register(RegistroRequest $request){
-    try{
+    public function register(RegistroRequest $request)
+    {
+        $g_response = Http::asForm()->post("https://www.google.com/recaptcha/api/siteverify", [
+            'secret' => env("CAPTCHA_SECRET_KEY"),
+            'response' => $request->input("g-recaptcha-response")
+        ]);
+        if (!$g_response->json('success')) {
+            return redirect()->route('registrarse')->with('error', 'No se ha validado el capcha!');
+        }
+
+        try {
             $user = User::create($request->validated());
             $user['password'] = Hash::make($request['password']);
-            Usuario::create(['fkiduser'=>$user->id]);
+            Usuario::create(['fkiduser' => $user->id]);
             auth()->login($user);
-
-        }
-        catch (QueryException $e){
-            $error_code=$e->errorInfo[1];
-            if($error_code==1062){
+        } catch (QueryException $e) {
+            $error_code = $e->errorInfo[1];
+            if ($error_code == 1062) {
                 //duplicado de email
-                return redirect()->route('registrarse')->with('error','La identificación ya esta registrada');
+                return redirect()->route('registrarse')->with('error', 'La identificación ya esta registrada');
             }
         }
 
-        return redirect()->route('datospostulante')->with('success','¡Usuario creado! Se ha iniciado sesion automaticamente.
+        return redirect()->route('datospostulante')->with('success', '¡Usuario creado! Se ha iniciado sesion automaticamente.
         Por favor ingrese la informacion de su perfil profesional');
-
     }
 }
--- resources/views/usuario/registrarse.blade.php
+++ resources/views/usuario/registrarse.blade.php
@@ -2,17 +2,18 @@
 
 @section('titulo')
     <title>Registrarse</title>
+    <script src="https://www.google.com/recaptcha/api.js"></script>
 @endsection
 
 @section('breadcrumb')
     <p>
         /<a class="breadcrumb"href="/">Página principal</a>
         @guest
-        /<a class="breadcrumb"href="/registrarse">Registrarse</a>
+            /<a class="breadcrumb"href="/registrarse">Registrarse</a>
         @endguest
         @auth
-        /<a class="breadcrumb"href="/admin/usuarios">Usuarios</a>
-        /<a class="breadcrumb"href="/admin/usuarios/registrar">Registrar usuarios con privilegio</a>
+            /<a class="breadcrumb"href="/admin/usuarios">Usuarios</a>
+            /<a class="breadcrumb"href="/admin/usuarios/registrar">Registrar usuarios con privilegio</a>
         @endauth
 
     </p>
@@ -26,70 +27,70 @@
         <div class="row p-2 ">
             <div class="col-md-10 mx-auto ">
                 @auth
-                @if(auth()->user()->privilegio==3)
-                    <form method="POST" action="/admin/usuarios/registrar" id="form-validation">
-                @endif
+                    @if (auth()->user()->privilegio == 3)
+                        <form method="POST" action="/admin/usuarios/registrar" id="form-validation">
+                    @endif
                 @endauth
                 @guest
-                   <form method="POST" action="/registrarse" id="form-validation">
-                @endguest
+                    <form method="POST" action="/registrarse" id="form-validation">
+                    @endguest
                     @csrf
                     <div class="form-group row">
                         <div class="col-sm-6 p-2">
                             <label for="nombre">Nombre</label>
-                            <input type="text" class="form-control" name="nombre"
-                            minlength="2" placeholder="Primer nombre" value="{{ old('nombre') }}" required>
+                            <input type="text" class="form-control" name="nombre" minlength="2"
+                                placeholder="Primer nombre" value="{{ old('nombre') }}" required>
                         </div>
                         <div class="col-sm-6 p-2">
                             <label for="apellido">Apellido</label>
-                            <input type="text" class="form-control" name="apellido"
-                            minlength="5" placeholder="Apellidos" value="{{ old('apellido') }}" required>
+                            <input type="text" class="form-control" name="apellido" minlength="5"
+                                placeholder="Apellidos" value="{{ old('apellido') }}" required>
                         </div>
                     </div>
                     <div class="form-group row">
                         <div class="col-sm-6 p-2">
                             <label for="email">Email</label>
                             <input type="text" class="form-control" name="email" value="{{ old('email') }}"
-                            placeholder="Email">
+                                placeholder="Email">
                         </div>
                         <div class="col-sm-6 p-2">
                             <label for="telefono">Teléfono</label>
-                            <input type="number" class="form-control" name="telefono"
-                            minlength="7" placeholder="Teléfono" value="{{ old('telefono') }}" required>
+                            <input type="number" class="form-control" name="telefono" minlength="7" placeholder="Teléfono"
+                                value="{{ old('telefono') }}" required>
                         </div>
                     </div>
                     @auth
-                    @if (auth()->user()->privilegio==3)
-                    <div class="form-group row">
-                        <div class="col-sm-6 p-2">
-                            <label for="privilegio">Privilegio</label>
-                            <select name="privilegio" class="form-select form-select-lg" onchange="cambiar(this)">
-                                <option value="1" selected>Usuario</option>
-                                <option value="2">Personal universitario</option>
-                                <option value="3">Administrator</option>
-                            </select>
-                        </div>
-                        <div class="col-sm-6 p-2" id="selUni" hidden>
-                            <label for="fkIdUni" class="form-label">Universidad</label>
-                            <select class="form-select form-select-lg" name="fkIdUni" id="fkIdUni">
-                                @foreach ($universidades as $u)
-                                <option value="{{ $u->idUniversidad }}">{{ $u->nombreUniversidad }}</option>
-                                @endforeach
-                            </select>
-                        </div>
-                    </div>
-                    @endif
+                        @if (auth()->user()->privilegio == 3)
+                            <div class="form-group row">
+                                <div class="col-sm-6 p-2">
+                                    <label for="privilegio">Privilegio</label>
+                                    <select name="privilegio" class="form-select form-select-lg" onchange="cambiar(this)">
+                                        <option value="1" selected>Usuario</option>
+                                        <option value="2">Personal universitario</option>
+                                        <option value="3">Administrator</option>
+                                    </select>
+                                </div>
+                                <div class="col-sm-6 p-2" id="selUni" hidden>
+                                    <label for="fkIdUni" class="form-label">Universidad</label>
+                                    <select class="form-select form-select-lg" name="fkIdUni" id="fkIdUni">
+                                        @foreach ($universidades as $u)
+                                            <option value="{{ $u->idUniversidad }}">{{ $u->nombreUniversidad }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                        @endif
                     @endauth
 
                     @guest
-                    <input type="number" name="privilegio" value="1" hidden>
+                        <input type="number" name="privilegio" value="1" hidden>
                     @endguest
 
                     <div class="form-group row">
                         <div class="col-sm-6 p-2">
                             <label for="direccion">Dirección</label>
-                            <input type="text" class="form-control" name="direccion"
-                            minlength="5" placeholder="direccion" value="{{ old('direccion') }}" required>
+                            <input type="text" class="form-control" name="direccion" minlength="5"
+                                placeholder="direccion" value="{{ old('direccion') }}" required>
                         </div>
                         <div class="col-sm-6 p-2">
                             <label for="ndoc">Documento</label>
@@ -107,14 +108,18 @@
                         <div class="col-sm-6 p-2">
                             <label for="password">Contraseña</label>
                             <input type="password" class="form-control" oninput="checkpass()" name="password"
-                            id="password" placeholder="contraseña"
-                            minlength="8" maxlength="16">
+                                id="password" placeholder="contraseña" minlength="8" maxlength="16">
                         </div>
                         <div class="col-sm-6 p-2">
                             <label for="password_confirmacion">Confirmar Contraseña</label>
                             <input type="password" class="form-control" oninput="checkpass()"
-                            name="password_confirmacion" id="password_confirmacion" placeholder="contraseña"
-                            minlength="8" maxlength="16">
+                                name="password_confirmacion" id="password_confirmacion" placeholder="contraseña"
+                                minlength="8" maxlength="16">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <div class="col-sm-6 p-2">
+                            <div class="g-recaptcha" data-sitekey={{ env('CAPTCHA_SITE_KEY') }}></div>
                         </div>
                     </div>
                     <h6 class="alert alert-success" id="success" hidden></h6>
@@ -130,52 +135,53 @@
 @endsection
 
 @section('scripts')
-<script>
-//const danger=document.getElementById("danger");
-const success=document.getElementById("success");
-const c1=document.getElementById("password");
-const c2=document.getElementById("password_confirmacion");
+    <script>
+        function onSubmit(token) {
+            document.getElementById("form-login").submit();
+        }
+        //const danger=document.getElementById("danger");
+        const success = document.getElementById("success");
+        const c1 = document.getElementById("password");
+        const c2 = document.getElementById("password_confirmacion");
 
-function checkpass(){
-    success.removeAttribute("hidden");
-    if(c1.value==c2.value){
-        success.setAttribute("class", "alert alert-success");
-        success.innerHTML = "Las contraseñas coinciden";
-        return true;
-    }
-    else{
-        success.setAttribute("class", "alert alert-danger");
-        success.innerHTML = "Las contraseñas NO coinciden";
-        return false;
-    }
-}
+        function checkpass() {
+            success.removeAttribute("hidden");
+            if (c1.value == c2.value) {
+                success.setAttribute("class", "alert alert-success");
+                success.innerHTML = "Las contraseñas coinciden";
+                return true;
+            } else {
+                success.setAttribute("class", "alert alert-danger");
+                success.innerHTML = "Las contraseñas NO coinciden";
+                return false;
+            }
+        }
 
-var form = document.getElementById("form-validation");
- form.addEventListener("submit", function(event) {
-      if ( !checkpass() ) {
-          //alert("Password mismatch");
-          event.preventDefault();
-          event.stopPropagation();
-      }
-      else if (form.checkValidity() == false) {
-          event.preventDefault();
-          event.stopPropagation();
-      }
-      form.classList.add("was-validated");
-      //c2.parentNode.removeChild(c2);
-    }, false);
+        var form = document.getElementById("form-validation");
+        form.addEventListener("submit", function(event) {
+            if (!checkpass()) {
+                //alert("Password mismatch");
+                event.preventDefault();
+                event.stopPropagation();
+            } else if (form.checkValidity() == false) {
+                event.preventDefault();
+                event.stopPropagation();
+            }
+            form.classList.add("was-validated");
+            //c2.parentNode.removeChild(c2);
+        }, false);
 
-@auth
-@if(auth()->user()->privilegio==3)
-function cambiar(sel){
-    console.log("cambiar");
-    if(sel.value==2){
-        document.getElementById("selUni").hidden=false;
-    } else {
-        document.getElementById("selUni").hidden=true;
-    }
-}
-@endif
-@endauth
-</script>
+        @auth
+        @if (auth()->user()->privilegio == 3)
+            function cambiar(sel) {
+                console.log("cambiar");
+                if (sel.value == 2) {
+                    document.getElementById("selUni").hidden = false;
+                } else {
+                    document.getElementById("selUni").hidden = true;
+                }
+            }
+        @endif
+        @endauth
+    </script>
 @endsection
